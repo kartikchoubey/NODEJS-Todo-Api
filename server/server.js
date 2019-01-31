c87fccc6f5027ce86f37dc777d1852b8a9f0e0ba
kartikchoubey <chaubeykartik@gmail.com>
2019-01-31 14:53:51 +0530

    initial commit 2

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express=require('express');
 var bodyParser=require('body-parser');
+var _=require('lodash');
 
 var {ObjectID}= require('mongodb');
 var {mongoose}=require('./db/mongoose');
@@ -7,6 +8,7 @@ var {Todo}=require("./models/todos");
 var {User}=require('./models/user');
 
 var app= express();
+const port=process.env.PORT || 3000 ;
 
 app.use(bodyParser.json());
 
@@ -45,8 +47,46 @@ app.get('/todos/:id',(req,res)=>{
       })
 })
 
-app.listen(3000,()=>{
-    console.log('Started on Port 3000');
+app.delete('/todos/:id',(req,res)=>{
+        var id =req.params.id
+        if(!ObjectID.isValid(id)){
+           return res.status(404).send();
+        }
+        Todo.findByIdAndRemove(id).then((todo)=>{
+            if(!todo){
+                return  res.status(404).send();
+            };
+            res.send(todo).status(200);
+        }).catch((e)=>{
+            res.status(400).send();
+        });
+});
+
+app.patch('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    var body =_.pick(req.body,['text','complete']);
+
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+    if(_.isBoolean(body.complete) && body.complete){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.complete=false
+        body.completedAt=null
+    }
+    Todo.findByIdAndUpdate(id,{$set : body},{new :true}).then((todo)=>{
+        if(!todo){
+            res.status(404).send();
+        }
+        res.send(todo);
+    }).catch((e)=>{
+        res.status(400).send();
+    })
+})
+
+app.listen(port,()=>{
+    console.log(`Started on Port ${port}`);
 });
 
 
